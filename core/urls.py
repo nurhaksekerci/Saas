6edb45649ae75d01a3714118d7dfe24fe303f669
Nurhak ŞEKERCİ <nurhaksekerci12@gmail.com>
2025-02-24 14:18:12 +0300

    Add static and media file serving for development environments

--- core/urls.py
+++ core/urls.py
@@ -30,6 +30,11 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
 
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+
 # Admin site başlığını özelleştir
 admin.site.site_header = 'SaaS Yönetim Paneli'
 admin.site.site_title = 'SaaS Admin'
